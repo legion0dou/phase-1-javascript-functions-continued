17d9c4a5cd300c0da5ebebcc985aa01394021904
user_name <email_id>
2023-09-26 11:43:47 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+// Implement a function saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Implement a function mondayWork
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // Implement a function wrapAdjective
+  function wrapAdjective(flair = "*") {
+    return function (adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  // Example usage
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("amazing")); // Output: "You are !!!amazing!!!"
+  
